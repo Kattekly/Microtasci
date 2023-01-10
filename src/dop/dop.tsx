c8a3331324a9b998e5d190ee07417d2936af4718
Ekaterina <kattekly@mail.ru>
2023-01-10 15:46:38 +0300

    repit 1

---
+++ src/dop/dop.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import {NewComponent} from "./NewComponent";
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+}
+
+type NewComponentType = Array<StudentType>
+
+function App(props: NewComponentType) {
+    const [students, setStudents] = useState([
+            {id: 1, name: "James", age: 8},
+            {id: 2, name: "Robert", age: 18},
+            {id: 3, name: "John", age: 28},
+            {id: 4, name: "Michael", age: 38},
+            {id: 5, name: "William", age: 48},
+            {id: 6, name: "David", age: 58},
+            {id: 7, name: "Richard", age: 68},
+            {id: 8, name: "Joseph", age: 78},
+            {id: 9, name: "Thomas", age: 88},
+            {id: 10, name: "Charles", age: 98},
+            {id: 11, name: "Christopher", age: 100},
+        ]
+    )
+    return (
+        <NewComponent students={students}/>
+    );
+}
+
+export default App;
