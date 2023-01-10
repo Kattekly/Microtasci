d36083a77848c27c58208958da8fd8f17e3f576c
Ekaterina <kattekly@mail.ru>
2023-01-10 16:24:52 +0300

    repit 1

--- src/dop/NewComponent.tsx
+++ src/dop/NewComponent.tsx
@@ -10,18 +10,40 @@ type NewComponentType = {
     students: Array<StudentType>
 }
 
+type CarsType = {
+    manufacturer: string
+    model: string
+}
 
-const NewComponent = (props: NewComponentType) => {
+const NewComponent = (props: Array<CarsType>) => {
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
     return (
-        <ul>
-            {props.students.map((el, id) => {
-                return (<li key={el.id}>
-                    <span>{el.name}</span>
-                    <span>{` ${el.age}`}</span>
-                </li>)
-            })}
-        </ul>
-    );
+        <table>
+            {props}
+
+            <tr>
+                <th></th>
+            </tr>
+        </table>
+    )
+
+
+
+    /* return (
+         <ul>
+             {props.students.map((el) => {
+                 return (
+                     <li key={el.id}>
+                         <span>{el.name}</span>
+                         <span>{` ${el.age}`}</span>
+                     </li>)
+             })}
+         </ul>
+     );*/
 };
 
 export default NewComponent;
