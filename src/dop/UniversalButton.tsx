457ab166c47ae7ddfe8080d6430058bd924ba4b7
Ekaterina <kattekly@mail.ru>
2023-01-11 15:27:02 +0300

    repit 2

--- src/dop/UniversalButton.tsx
+++ src/dop/UniversalButton.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 
 type UniversalButtonType = {
     name: string
+    callback: () => void
 }
 
 const UniversalButton = (props: UniversalButtonType) => {
 
     const onClickHandler = () => {
-
+        props.callback
     }
 
     return (
