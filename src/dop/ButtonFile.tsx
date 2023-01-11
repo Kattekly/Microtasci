0bc4403d15c92895dfd690e8384dbb2067178ff6
Ekaterina <kattekly@mail.ru>
2023-01-11 15:32:27 +0300

    repit 2

--- src/dop/ButtonFile.tsx
+++ src/dop/ButtonFile.tsx
@@ -3,18 +3,18 @@ import UniversalButton from "./UniversalButton";
 
 const ButtonFile = () => {
 
-    const ButFoo1 = () => {
-        alert('11111')
+    const ButFoo1 = (sub: string) => {
+        alert(sub)
     }
 
-    const ButFoo2 = () => {
-        alert('22222')
+    const ButFoo2 = (sub: string) => {
+        alert(sub)
     }
 
     return (
         <div>
-            <UniversalButton name={'My'} callback={ButFoo1}/>
-            <UniversalButton name={'You'} callback={ButFoo2}/>
+            <UniversalButton name={'My'} callback={() => ButFoo1('1111')}/>
+            <UniversalButton name={'You'} callback={() => ButFoo2('2222')}/>
         </div>
     );
 };
