680a6fbc7b1df613084322fcf516ebc6d16bc288
Ekaterina <kattekly@mail.ru>
2023-01-11 14:57:36 +0300

    repit 2

--- src/dop/ButtonFile.tsx
+++ src/dop/ButtonFile.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const ButtonFile = () => {
-    const FirstSubs = (event: React.MouseEvent<HTMLButtonElement>) => {
+/*    const FirstSubs = (event: React.MouseEvent<HTMLButtonElement>) => {
         alert('Hello Yo')
     }
 
     const FirstSubs2 = (event: React.MouseEvent<HTMLButtonElement>) => {
         alert('Hello Marry')
+    }*/
+
+    const onClickHandler = (name: string) => {
+        alert(name)
     }
 
     return (
         <div>
            {/* <button onClick={(event)=>{alert('Hello')}}>MyChanel-1</button>*/}
-            <button onClick={FirstSubs}>MyChanel-1</button>
-            <button onClick={FirstSubs2}>MyChanel-2</button>
+            <button onClick={() => {onClickHandler('Dasha')}}>MyChanel-1</button>
+            <button onClick={() => {onClickHandler('Dima')}}>MyChanel-2</button>
         </div>
     );
 };
