81a4f9c170625c24ac99c842b2be6aa62b4d0324
Ekaterina <kattekly@mail.ru>
2023-01-12 10:42:00 +0300

    repit 3

--- src/dop/FilterFile.tsx
+++ src/dop/FilterFile.tsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 
 const FilterFile = () => {
+    type FilterType = "ALL" | "Dollars" | "RUBLS"
+
     const [money, setMoney] = useState([
         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
@@ -11,12 +13,17 @@ const FilterFile = () => {
         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
     ])
+    const [filter, setFilter] = useState<FilterType>()
+    let currentMoney = money
+    if (filter === 'RUBLS') {
+        currentMoney = money.filter((m) => m.banknots === 'RUBLS')
+    }
+    if (filter === 'Dollars') {
+        currentMoney = money.filter((m) => m.banknots === 'Dollars')
+    }
 
-
-    let currentMoney = money.filter((m) => m.banknots === 'RUBLS')
-
-    const onClickHandler = () => {
-
+    const onClickHandler = (name: FilterType) => {
+        setFilter(name)
     }
 
     return (
@@ -33,9 +40,9 @@ const FilterFile = () => {
                 })}
             </ul>
             <div style={{marginLeft: "35px"}}>
-                <button onClick={onClickHandler}>ALL</button>
-                <button onClick={onClickHandler}>RUBLS</button>
-                <button onClick={onClickHandler}>DOLLARS</button>
+                <button onClick={() => onClickHandler('ALL')}>ALL</button>
+                <button onClick={() => onClickHandler('RUBLS')}>RUBLS</button>
+                <button onClick={() => onClickHandler('Dollars')}>DOLLARS</button>
             </div>
         </>);
 };
